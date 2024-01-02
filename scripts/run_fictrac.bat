c618fb86b4bf09b9e2ca85ed6c93fdb57bb5db44
Ali Hadi Shenasa <38480296+AliShenasa@users.noreply.github.com>
2024-01-02 15:32:04 -0800

    Update config to save video files
    Also updated fictrac save location to NAS.  (Z:\Live Fly Imaging data\fictrac)

--- scripts/run_fictrac.bat
+++ scripts/run_fictrac.bat
@@ -1,6 +1,11 @@
 @echo off
 rem Script to run fictrac
-cd %~dp0
-cd ..\TEsetup
-call ..\bin\Release\fictrac.exe config.txt
+:: Connect to NAS if it is not available
+if not exist z:\ (net use z: "\\TurnerEvans-NAS\Lab Storage" /user:ahshenas cupcake)
+:: change directory to NAS
+Z:
+:: location to store data
+cd "Z:\Live Fly Imaging data\fictrac"
+:: call fictrac
+call C:\Users\exx\Documents\GitHub\fictrac\bin\Release\fictrac.exe C:\Users\exx\Documents\GitHub\fictrac\fictracCalibrationFiles\config.txt
 pause
